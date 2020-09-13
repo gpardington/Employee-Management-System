9e775a839b476f938ff22e94d3eb26c623a057e0
Grant Pardington <grant.pardington@gmail.com>
2020-09-13 11:39:01 -0600

    Establishing connection to mySQL

--- server.js
+++ server.js
@@ -4,6 +4,25 @@ const inquirer = require("inquirer");
 const console_table = require("console.table");
 const clear = require("console-clear");
 const util = require("util");
+const { start } = require("repl");
 
 const employeesSQL = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.department_name, concat(manager.first_name," ", manager.last_name) as manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee as manager on employee.manager_id = manager.id';
+
+//Establish connection to mySQL database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employees_DB",
+});
+
+//Async/Await concept connection
+connection.query = util.promisify(connection.query);
+
+//Connect to mySQL server and database
+connection.connect(function(err) {
+    if (err) throw err;
+    start();
+});
 
